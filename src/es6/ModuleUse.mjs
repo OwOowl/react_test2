113b43857e9ef5a9d2e31a3122cad607e80b6cd1
yang <ymh0052@naver.com>
2022-12-29 16:02:27 +0900

    모듈

--- src/es6/ModuleUse.mjs
+++ src/es6/ModuleUse.mjs
@@ -6,7 +6,7 @@ import sub from './Mymod2.mjs';
 console.log(name);
 console.log(job);
 console.log(gender);
-console.log(getInfo());
+getInfo();
 
 console.log(`첫 번째 숫자 : ${num1} + 두 번째 숫자 : ${num2} 의 값 : ${sum()}`);
 console.log(`sub() 실행 : ${sub()}`);
